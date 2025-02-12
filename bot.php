5392ea55d296ec8783bd9db5aed1862dc846b272
phantom <143726462+phantom-666-666@users.noreply.github.com>
2025-02-12 17:17:09 +0330

    bot.php

--- bot.php
+++ bot.php
@@ -1,17 +1,17 @@
 <?php 
- $update = file_get_contents('php://input');
- file_put_contents ('bot.txt', $update); 
+$update = file_get_contents('php://input');
+file_put_contents('bot.txt', $update); 
 $update = json_decode($update, TRUE); 
- $message = $update ['message']['text']; 
-$chatID = $update ['message']['from']['id'];
- $token = '7882198041:AAHYwYyCvrc4qfMzvzoVc_JftdY4-PtfsJc';
-   function sendMessage ($chatid, $text)
+$message = $update['message']['text']; 
+$chatID = $update['message']['from']['id'];
+ 
+function sendMessage($chatid, $text)
 { 	
 $url = 'https://api.telegram.org/bot7882198041:AAHYwYyCvrc4qfMzvzoVc_JftdY4-PtfsJc/sendMessage?chatid='.$chatid."&text=".$text;
 file_get_contents($url);
  }
   
-if($message == '/start') 
+if($message=='/start') 
 { 	
 sendMessage($chatID, 'Hi'); 
 } 
